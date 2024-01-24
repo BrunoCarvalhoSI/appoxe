bee8d029218990fa7adf7dad76cef401805840da
PC-GAMER\bruno <bruno.oliveira@itsolved.com.br>
2024-01-24 19:55:49 -0300

    Trocando tudo pra getX

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:oxesushi_v1/pages_routes/app_pages.dart';
-import '../screens/screen_signIn.dart';
 import 'componentes/custom_colors.dart';
-import 'splash/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
